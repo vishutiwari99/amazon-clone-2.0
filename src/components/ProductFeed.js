4ecf4a40f5738b1de7ee9ed32e8e9749aa6a522c
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-05-29 07:21:36 +0530

    All folder Structure Changed and added Various components

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,22 @@
+import Product from "./Product"
+
+const ProductFeed = ({ products }) => {
+    return (
+        <div>
+            <h1>Products</h1>
+            {products.map(({ id, title, price, description, category, image }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default ProductFeed
