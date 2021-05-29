af69a334ce388ce56b4ba196f80266e7b40f35d7
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-05-29 22:06:24 +0530

    Added firebase config

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAOEWtx9nYsnyBrJfIMoZ3ewmG4B15DYzE",
+    authDomain: "fir-dcc48.firebaseapp.com",
+    projectId: "fir-dcc48",
+    storageBucket: "fir-dcc48.appspot.com",
+    messagingSenderId: "758274857169",
+    appId: "1:758274857169:web:1991dbb7edf5932f6e4df0",
+    measurementId: "G-DX1C6QD4EB"
+};
