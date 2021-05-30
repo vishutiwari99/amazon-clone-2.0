a743324d455b55ef8df8ea9d89f529331f82c82d
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-05-30 22:48:47 +0530

    Bug fixed

--- src/components/Product.js
+++ src/components/Product.js
@@ -21,15 +21,13 @@ const Product = ({ id, title, price, description, category, image }) => {
 
     const [hasPrime] = useState(Math.random() < 0.5)
     return (
-        <div key={id} className="relative flex flex-col m-5 bg-white z-30 p-10">
+        <div className="relative flex flex-col m-5 bg-white z-30 p-10">
             <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
-
-
             <Image src={image} height={200} width={200} objectFit="contain" />
             <h4 className="my-3">{title}</h4>
             <div key={id} className="flex">
                 {Array(rating).fill().map((_, i) => (
-                    <StarIcon key={_} className="h-5 text-yellow-500" />
+                    <StarIcon key={i} className="h-5 text-yellow-500" />
                 ))}
             </div>
 
